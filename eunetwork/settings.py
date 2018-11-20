0bb2a3a2cc6378d6b0b8b308ffc4b3a8ef35c232
pirrencode <pirrencode@gmail.com>
2018-11-20 15:07:34 +0200

    Created events app and Event model

--- eunetwork/settings.py
+++ eunetwork/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'posts'
+    'posts',
+    'events'
 ]
 
 MIDDLEWARE = [
---
+++ events/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.TextField()
+    icon = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%d.%m.%Y')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]+'...'
